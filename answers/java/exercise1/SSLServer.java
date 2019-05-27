561a66f001fce667e04a55e716e0dc685c8b4b1d
Michael Pound <Michael.Pound@nottingham.ac.uk>
2019-05-27 16:08:11 +0100

    Adjusted java cipher suites

--- answers/java/exercise1/SSLServer.java
+++ answers/java/exercise1/SSLServer.java
@@ -36,7 +36,7 @@ public class SSLServer {
             // Part 2: You can add code here to limit the protocols and suites the server may use
             // Note: If the server and client don't share at least one cipher suite in common,
             // you can expect the handshake to fail.
-            socket.setEnabledCipherSuites(new String[]{ "TLS_AES_128_GCM_SHA256", "TLS_AES_256_GCM_SHA384", "TLS_CHACHA20_POLY1305_SHA256" });
+            socket.setEnabledCipherSuites(new String[]{ "TLS_AES_128_GCM_SHA256", "TLS_CHACHA20_POLY1305_SHA256", "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256" });
             socket.setEnabledProtocols(new String[]{ "TLSv1.3", "TLSv1.2" });
 
             System.out.println(String.format("Listening on port %d...", socket.getLocalPort()));
